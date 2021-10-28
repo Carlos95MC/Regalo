ce1aeb6464ecbabff5c065cf12af259211e95058
Carlos95MC <60006672+Carlos95MC@users.noreply.github.com>
2021-10-28 19:09:28 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+ <script>
+     function Agregar_Click() {
+
+     }
+</script>
+        <div class="row"></div>
+    <div class="row">
+         <div class="col-md-3">
+             Cantidad:
+         </div>
+        <div class="col-md-4">
+            
+            <asp:TextBox id="idCantidad" runat="server" />
+        </div>
+    </div>    
+    <div class="row"></div>
+    <div class="row">   
+         <div class="col-md-3">
+              Nombre Producto:
+         </div>
+        <div class="col-md-4">           
+           <asp:TextBox id="idNombreProducto" runat="server" />
+        </div>        
+    </div>
+    <div class="row"></div>
+    <div class="row"> 
+        <div class="col-md-3">
+                Precio Unitario:
+         </div>
+        <div class="col-md-4">          
+           <asp:TextBox id="idPrecio" runat="server" />
+        </div>
+    </div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row">
+        <div class="col-md-2">
+            <asp:DropDownList runat="server" ID="ddlLista" />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2">
+            <asp:Button runat="server" id="BtnAgregar" Text="Agregar" OnClick="Agregar_Click"/>
+        </div>
+    </div>
+    <div class="row">
+        Favoritos: <asp:Label ID="lbFavoritos" runat="server"/>
+    </div>
+    <div class="row">
+        Carrito: <asp:Label ID="lbCarrito" runat="server"/>
+    </div>
+    <div class="row">
+        Suma: <asp:Label ID="sumaSubtotal" runat="server"/>
+    </div>
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        ListItem listItem;
+                
+        decimal precio;
+        int count1 = 0;
+        int count2 = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ddlLista.Items.Count == 0)
+            {
+                listItem = new ListItem("Favoritos", "1");
+                ddlLista.Items.Add(listItem);
+                listItem = new ListItem("Carrito", "2");
+                ddlLista.Items.Add(listItem);
+            }
+        }
+        protected void Agregar_Click(Object sender, EventArgs e)
+        {
+            if (ddlLista.SelectedValue.Equals("1"))
+            {
+                if(lbFavoritos.Text.Equals(""))
+                    lbFavoritos.Text =  idNombreProducto.Text + " " + idPrecio.Text+ "€";
+                else
+                    lbFavoritos.Text = lbFavoritos.Text + ", " + idNombreProducto.Text + " " + idPrecio.Text + "€";
+                count1++;
+
+            }
+            if (ddlLista.SelectedValue.Equals("2"))
+            {
+                if (lbCarrito.Text.Equals(""))
+                    lbCarrito.Text = idCantidad.Text + " X " + idNombreProducto.Text +" "+ idPrecio.Text + "€" + " Subtotal item: " + decimal.Parse(idPrecio.Text) * decimal.Parse(idCantidad.Text) + "€";
+                else
+                    lbCarrito.Text = lbCarrito.Text + ", " + idCantidad.Text + " X " + idNombreProducto.Text + " " + idPrecio.Text + "€" + " Subtotal item: " + decimal.Parse(idPrecio.Text) * decimal.Parse(idCantidad.Text) + "€";
+                count2++;
+
+                precio = precio + (decimal.Parse(idPrecio.Text) * decimal.Parse(idCantidad.Text));
+                sumaSubtotal.Text = precio + "€";
+            }
+           
+        }
+    }
+}
